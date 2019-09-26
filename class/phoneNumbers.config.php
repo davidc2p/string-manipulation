cf3b9d19b676708356838f75a0599b651fdcd20c
David Domingues <0101046@PT.insags.com>
2019-09-26 17:08:43 +0100

    New method isEmail

--- class/phoneNumbers.config.php
+++ class/phoneNumbers.config.php
@@ -1,8 +1,10 @@
 <?php
 //Define pattern for each country according to https://en.wikipedia.org/wiki/National_conventions_for_writing_telephone_numbers
+//Complete this array for more countries. This is supplied as sample only.
 return array(
     array(
         'country' => 'PT', 
+        'countryName' => 'Portugal', 
         'countrySeparator' => '()',
         'charSeparators' => array('-', '+', ' '), 
         'numCountry' => '351', 
@@ -11,7 +13,28 @@ return array(
         'maxLength' => 10
     ),
     array(
+        'country' => 'BE', 
+        'countryName' => 'Belgium', 
+        'countrySeparator' => '()',
+        'charSeparators' => array('-', '+', ' ', '/', '.'), 
+        'numCountry' => '32', 
+        'acceptedMask' => array('/0[0-9]{8}$/', '/04[0-9]{8}$/', '/4[0-9]{8}$/'), 
+        'minLength' => 9, 
+        'maxLength' => 10
+    ),
+    array(
+        'country' => 'DE', 
+        'countryName' => 'Germany', 
+        'countrySeparator' => '()',
+        'charSeparators' => array('-', '+', ' '), 
+        'numCountry' => '49', 
+        'acceptedMask' => array('/0[0-9]{10}$/', '/0[0-9]{12}$/', '/[0-9]{10}$/'), 
+        'minLength' => 10, 
+        'maxLength' => 12
+    ),
+    array(
         'country' => 'FR', 
+        'countryName' => 'France', 
         'countrySeparator' => '()',
         'charSeparators' => array('-', '+', ' '), 
         'numCountry' => '33', 
--- test.php
+++ test.php
@@ -6,30 +6,49 @@
 
     $phoneNumber = "(+351)21-851 82 52";
     $a = new lib\ValidateString();
-    //validate if phone number is from portugal
-    $b = $a->isPhoneNumber($phoneNumber, false, 'PT');
-    print 'validate if phone number is from portugal: ';
-    var_dump ($b);
+    //Validate if phone number is from portugal
+    // $b = $a->isPhoneNumber($phoneNumber, false, 'PT');
+    // print '<br>Validate if phone number '.$phoneNumber.' is from portugal: ';
+    // var_dump ($b);
 
-    //validate if phone number is from france
-    $b = $a->isPhoneNumber($phoneNumber, false, 'FR');
-    print 'validate if phone number is from France: ';
-    var_dump ($b);
+    //Validate if phone number is from france
+    // $b = $a->isPhoneNumber($phoneNumber, false, 'FR');
+    // print '<br>Validate if phone number '.$phoneNumber.' is from France: ';
+    // var_dump ($b);
 
-    //validade if phone number is from any country
+    //Validate if phone number is from any country
+    // $b = $a->isPhoneNumber($phoneNumber, false);
+    // print '<br>Validate if phone number '.$phoneNumber.' is from any country: ';
+    // var_dump ($b);
+
+    //Validate if phone number is from any country without country code (not found)
+    $phoneNumber = "51-851-82-5273";
     $b = $a->isPhoneNumber($phoneNumber, false);
-    print 'validade if phone number is from any country: ';
+    print '<br>Validate if phone number '.$phoneNumber.' is from any country without country code: ';
     var_dump ($b);
 
-    //validade against a specific pattern (begins with 71 or 72 and 9 digits long)
-    $b = $a->isPhoneNumber($phoneNumber, false, 'PT', array('acceptedMask' => array('/7[12][0-9]{7}$/')));
-    print 'validade against a specific pattern (begins with 71 or 72 and 9 digits long): ';
+    //Validate if phone number is from any country without country code (found)
+    $phoneNumber = "01-851-82-527";
+    $b = $a->isPhoneNumber($phoneNumber, false);
+    print '<br>Validate if phone number '.$phoneNumber.' is from any country without country code: ';
     var_dump ($b);
 
-    //validade is phone number really exists
-    $b = $a->isPhoneNumber($phoneNumber, true);
-    print 'validade is phone number really exists: ';
+    //Validate if phone number is from Belgium (found)
+    $phoneNumber = "(+32)455/118.252";
+    $b = $a->isPhoneNumber($phoneNumber, false);
+    print '<br>Validate if phone number '.$phoneNumber.' is from any country without country code: ';
     var_dump ($b);
+
+    //Validate against a specific pattern (begins with 71 or 72 and 9 digits long)
+    $b = $a->isPhoneNumber($phoneNumber, false, null, array('acceptedMask' => array('/7[12][0-9]{7}$/')));
+    print '<br>Validate phone number '.$phoneNumber.' against a specific pattern (begins with 71 or 72 and 9 digits long): ';
+    var_dump ($b);
+
+    //Validate is phone number really exists
+    // $phoneNumber = "(351)21-851-82-52";
+    // $b = $a->isPhoneNumber($phoneNumber, true);
+    // print '<br>Validate is phone number '.$phoneNumber.' really exists: ';
+    // var_dump ($b);
 
 
 ?>
